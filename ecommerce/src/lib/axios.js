1d1d4ba51175b74fd4f245aa012ddda8c7a9b4d9
Toko Migineishvili <toko.migine@gmail.com>
2025-12-14 14:12:50 +0400

    Update axios.js

--- ecommerce/src/lib/axios.js
+++ ecommerce/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'advanced-ecommerce-ruby.vercel.app/api'
+    baseURL: 'https://advanced-ecommerce-ruby.vercel.app/api'
 })
 
 instance.interceptors.request.use((config) => {
